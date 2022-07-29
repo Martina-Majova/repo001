3f9300e3c62d73c8c966e1454ebe8094d3621e51
Martina Majova <45810973+Martina-May@users.noreply.github.com>
2022-07-29 22:07:03 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World!");
+	    for(int i=0; i>3; i++){
+		    System.out.println("Hello World!");
+	    }    
     }
 }
